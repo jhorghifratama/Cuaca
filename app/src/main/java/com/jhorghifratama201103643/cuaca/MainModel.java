5a89c61ace041156efb204744d9b23bc979aa5ec
jhorghifratama <jhorghifratama@gmail.com>
2023-06-23 14:48:42 +0700

    cuaca

---
+++ app/src/main/java/com/jhorghifratama201103643/cuaca/MainModel.java
@@ -0,0 +1,25 @@
+package com.jhorghifratama201103643.cuaca;
+
+public class MainModel {
+    private double temp_min;
+    private double getTemp_max;
+
+    public MainModel() {
+    }
+
+    public double getTemp_min() {
+        return temp_min;
+    }
+
+    public void setTemp_min(double temp_min) {
+        this.temp_min = temp_min;
+    }
+
+    public double getGetTemp_max() {
+        return getTemp_max;
+    }
+
+    public void setGetTemp_max(double getTemp_max) {
+        this.getTemp_max = getTemp_max;
+    }
+}
